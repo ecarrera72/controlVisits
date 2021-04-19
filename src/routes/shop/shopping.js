4570ed7205f5767858ef52932fee5fb2e8ec84d6
lalo <carrerag.eduardo3@gmail.com>
2021-04-19 18:56:38 -0500

    modificaciones para paginacion

--- src/routes/shop/shopping.js
+++ src/routes/shop/shopping.js
@@ -3,16 +3,21 @@ const { apiRest, getAuth } = require('../../service/api');
 const express = require('express');
 const router = express.Router();
 
-router.get( "/",  isloggedIn, async ( req, res ) => {
+router.get( "/:idCategory/",  isloggedIn, async ( req, res ) => {
     try {
-        categories = await apiRest( 'get', 'category', null, null, req.app.locals.token )
-        articles = await apiRest( 'get', 'article', null, null, req.app.locals.token );
-        res.render('shop/shopping', { articles: articles.data, categories: categories.data });
+        categories = await apiRest( 'get', 'category', null, null, req.app.locals.token );
+        articles = await apiRest( 'get', 'article', { page: 0, size: 15 }, { idCategory: req.params.idCategory },
+                                req.app.locals.token );
+        // console.log(articles.data.total);
+        // console.log(articles.data.page);
+        // console.log(articles.data.size);
+        // console.log( parseInt(articles.data.total / articles.data.size) + 1 );
+        res.render('shop/shopping', { articles: articles.data.items, categories: categories.data });
     } catch (error) {
-        console.error(error.stau);
-        switch (error.status) {
+        console.error(error.response);
+        switch (error.response.status) {
             case 404:
-                response = { data: null }    
+                response = { data: null }
                 break;
             case 401:
                 token = await getAuth();
